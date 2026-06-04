c8e19b378ff8b8c0d94115c4ccaa9ab11d93a1df
Sidnei Vieira de Souza <souzasidnei89@hotmail.com>
2026-06-03 21:52:59 -0300

    Add new routes for Times, Jogos, and Campeonatos

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import Times from "./pages/Times";
+import Jogos from "./pages/Jogos";
+import Campeonatos from "./pages/Campeonatos";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -16,6 +19,9 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/times" element={<Times />} />
+          <Route path="/jogos" element={<Jogos />} />
+          <Route path="/campeonatos" element={<Campeonatos />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
